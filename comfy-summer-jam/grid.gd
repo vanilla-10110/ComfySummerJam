b8e47b229efeccb61a41710d7bff9f0722907f65
Starla <Starlabelrose@gmail.com>
2026-06-09 16:23:51 +1000

    made the start of the main grid/ tilemap thingggy for building placment

--- comfy-summer-jam/grid.gd
+++ comfy-summer-jam/grid.gd
@@ -1,11 +1,50 @@
 extends Node2D
 
-const TILE_SIZE: Vector2 = Vector2(16, 16)
-# Called when the node enters the scene tree for the first time.
+@onready var ground = $ground
+@onready var preview = $preview
+
+var selected_tile : Vector2i
+
+var select_mode : bool = false
+var preview_tile : Vector2i:
+	set(value):
+		if preview_tile == value:
+			return
+		preview.erase_cell(preview_tile)
+		preview_tile = value
+		preview.set_cell(value, 1, selected_tile)
+	
+
+var GridSize_y = 5
+var GridSize_x = 8
+var Dic = {}
+
 func _ready() -> void:
-	pass # Replace with function body.
+	
+	
+	for x in GridSize_x:
+		for y in GridSize_y:
+			Dic[str(Vector2(x,y))] = {
+				"Type": "buildable_tile"
+			}
+			
+			ground.set_cell(Vector2(x, y), 0, Vector2i(0,0), 0)
+	print(Dic)
+	
+	select_mode = true
+	
+	
+	
 
+func snap_pos(global_pos: Vector2) -> Vector2i:
+	var local_pos = ground.to_local(global_pos)
+	var tile_pos = ground.local_to_map(local_pos)
+	
+	return tile_pos
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var working_space = ground.get_used_cells()
+	var working_tile = get_global_mouse_position()
+	if select_mode and working_space.has(snap_pos(get_global_mouse_position())):
+		preview_tile = snap_pos(get_global_mouse_position())
+	
