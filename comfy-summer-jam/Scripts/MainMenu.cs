5aa45b9d99fab123413d7c056ad92f02eeacad41
Bungitor <bungi@minmage.games>
2026-06-12 13:02:15 +1000

    menu buttons do things
    start button loads the main scene, settings button clears the main menu ready for the settings menu to appear, quit button closes the application

--- comfy-summer-jam/Scripts/MainMenu.cs
+++ comfy-summer-jam/Scripts/MainMenu.cs
@@ -3,21 +3,46 @@ using System;
 
 public partial class MainMenu : Control
 {
+
+
+	[Export]
+	private Button _startButton;
+	[Export]
+	private Button _settingsButton;
+	[Export]
+	private Button _quitButton;
+	[Export]
+	private CanvasItem _mainMenuCanvas;
+	
+
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_startButton.Pressed += StartButtonPressed;
+		_settingsButton.Pressed += SettingsButtonPressed;
+		_quitButton.Pressed += QuitButtonPressed;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//on_start_pressed(
-		GD.Print("bingus");
+	}
+
+	private void StartButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Main_Scene.tscn");
 
 	}
-	
-	
-		
-	
-	
+	private void SettingsButtonPressed()
+	{
+		_mainMenuCanvas.Visible= false;
+	}
+	private void QuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+
+
 }
